9121684fd9f4c67603f0e85161c75ca1cc1711d4
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-01 11:39:42 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:idnum, :name_kanji, :name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:idnum])
+  end
 end
+# , :password, :password_confirmation
+# , :password, :password_confirmation
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        #  def self.find_first_by_auth_conditions(warden_conditions)
+        #   conditions = warden_conditions.dup
+        #   if login = conditions.delete(:login)
+        #     # 認証の条件式を変更する
+        #     where(conditions).where(["idnum = :value", {:value => idnum}]).first
+        #   else
+        #     where(conditions).first
+        #   end 
+        # end 
 end
